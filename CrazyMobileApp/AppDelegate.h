c991ec8b23da75435a44b6a73a0945f424229281
Ｋｏｄｕｒｉ　ＭａｎａｓａＰｒｉｙａ <mkoduri@mbp-15uac-295.local>
2015-12-14 13:29:02 +0900

    Initial Commit

---
+++ CrazyMobileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyMobileApp
+//
+//  Created by Ｋｏｄｕｒｉ　ＭａｎａｓａＰｒｉｙａ on 12/14/15.
+//  Copyright © 2015 Ｋｏｄｕｒｉ　ＭａｎａｓａＰｒｉｙａ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrazyMobileApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyMobileApp
+//
+//  Created by Ｋｏｄｕｒｉ　ＭａｎａｓａＰｒｉｙａ on 12/14/15.
+//  Copyright © 2015 Ｋｏｄｕｒｉ　ＭａｎａｓａＰｒｉｙａ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
